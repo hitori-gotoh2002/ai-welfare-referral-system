7ecac3349338c563f0396fe3c9600db7cc01ba42
hitori-gotoh2002 <98andrew18@gmail.com>
2026-05-24 02:41:29 +0900

    Load LLM enhancement patch in server entry

--- server_entry.py
+++ server_entry.py
@@ -1,12 +1,14 @@
 import backend_runtime_patch
 import recommendation_relevance_patch
 import detail_alias_patch
+import llm_enhancement_patch
 import backend_server
 
 
 backend_runtime_patch.apply()
 recommendation_relevance_patch.apply()
 detail_alias_patch.apply()
+llm_enhancement_patch.apply()
 
 
 if __name__ == "__main__":

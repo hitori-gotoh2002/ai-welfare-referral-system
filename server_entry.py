f178e6d51f1090a4985cccb96fa4a90807878df7
hitori-gotoh2002 <98andrew18@gmail.com>
2026-05-26 19:10:46 +0900

    Apply commercial UI route patch

--- server_entry.py
+++ server_entry.py
@@ -5,6 +5,7 @@ import llm_enhancement_patch
 import rich_report_patch
 import welfare_link_patch
 import welfare_feature_patch
+import commercial_ui_route_patch
 import backend_server
 
 
@@ -15,6 +16,7 @@ llm_enhancement_patch.apply()
 rich_report_patch.apply()
 welfare_link_patch.apply()
 welfare_feature_patch.apply()
+commercial_ui_route_patch.apply()
 
 
 if __name__ == "__main__":

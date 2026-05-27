311d3523172ee8a042453bf9c9b45a1a7270a769
hitori-gotoh2002 <98andrew18@gmail.com>
2026-05-28 01:54:08 +0900

    Rollback welfare feature startup hook

--- server_entry.py
+++ server_entry.py
@@ -4,7 +4,6 @@ import detail_alias_patch
 import llm_enhancement_patch
 import rich_report_patch
 import welfare_link_patch
-import welfare_feature_patch
 import commercial_ui_route_patch
 import backend_server
 
@@ -15,7 +14,6 @@ detail_alias_patch.apply()
 llm_enhancement_patch.apply()
 rich_report_patch.apply()
 welfare_link_patch.apply()
-welfare_feature_patch.apply()
 commercial_ui_route_patch.apply()
 
 
